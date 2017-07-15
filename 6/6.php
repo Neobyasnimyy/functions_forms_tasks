9a87f2df24cee323d1b8936eeb54522e99b51fb5
Borysiuk Andrey <factodessa@gmail.com>
2017-07-16 00:18:45 +0300

    6 добавили проверку на Mime type and file size

--- 6/6.php
+++ 6/6.php
@@ -3,19 +3,32 @@
 @mkdir("gallery", 0777);// создаем новую директорию
 $uploadDir = './gallery/'; // путь к нашей новой директории
 if (!empty($_POST)===true) {
+    $message='';
 
     $arr = explode(".", $_FILES['image']['name']);
     $format = ["jpg","png","bmp","gif","jpeg"];
+    $size = $_FILES['image']['size'];
+    $type = $_FILES['image']['type'];
+    if(in_array($arr[count($arr)-1], $format)!==true) {// проверяем формат файлов, соответствует нашему списку
+        $message.= "<h4>Не правильный формат</h4><br>";
+    }
+    if ($size>2*1024*1024){
+        $message.= "<h4>Превышен объем загружаемой картинки.</h4><br>Допустимый 2Мб.<br>";
+    }
+    // проверяем тип файла
+    if (($type != "image/jpg") && ($type != "image/jpeg") && ($type !="image/gif")&&($type !="image/png")&&($type !="image/vnd.microsoft.icon")&&($type !="image/pjpeg")){
+        $message .= "<h4>Не правильный тип файла</h4><br>";
+    }
+    // создаем путь к новому файлу и добавляем в новае название рандомные чила, чтобы названия файлов не повторялись
+    $uploadFile = $uploadDir .(string)rand(0,1000)."-". basename($_FILES['image']['name']);
 
-    if(in_array($arr[count($arr)-1], $format)) {// проверяем формат файлов, соответствует нашему списку
-        $uploadFile = $uploadDir . basename($_FILES['image']['name']);// создаем путь к новому файлу
-        // картинка сохранена во временной директории, необходима ее скопировать
-        if (copy($_FILES['image']['tmp_name'], $uploadFile)){
-            $message = "<h3>Файл загружен</h3>";
+    // картинка сохранена во временной директории, необходима ее скопировать
+    if (empty($message)===true) {
+        // отправляем файл
+        if (copy($_FILES['image']['tmp_name'], $uploadFile)) {
+            $message .= "<h4>Файл загружен</h4><br>";
         }
     }
-    else
-        $message= "<h3>Не правильный формат</h3>";
 }
 
 ?>
@@ -24,46 +37,53 @@ if (!empty($_POST)===true) {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Title</title>
+    <title>Gallery</title>
     <style>
         body{
             text-align: center;
         }
         img{
-            max-height: 200px;
-            max-width: 200px;
+            height: 200px;
+            max-width 200px;
+        }
+        table{
+            margin: 10px auto;
         }
     </style>
 </head>
 <body>
 <br>
 <?php if (!empty($message)){
-    print "<p>$message</p>";
+    print $message;
 }
 ?>
-<br>
-    <form action="6.php" method="POST" enctype="multipart/form-data">
-        <h3>Добавить картинку</h3>
-        <br>
-        <p><input type="file" name="image" accept="image/*"></p>
-        <p><input type="submit" value="Загрузить" name="submit"></p>
-    </form>
-    <!--проверяем если есть наша функция то получаем массив элементов в нем-->
-    <?php if((is_dir($uploadDir)) and ($arr =scandir($uploadDir))): ?>
-        <table>
-            <tr>
-            <?php foreach($arr as $item): ?>
-                    <td>
-                        <?php if (!is_dir($uploadDir.$item)){
-                            if (exif_imagetype($uploadDir.$item)!=false){
-                                echo '<img src="'.$uploadDir.$item.'" >';
-                            }
+<form action="6.php" method="POST" enctype="multipart/form-data">
+    <h3>Добавить картинку</h3>
+    <p><input type="file" name="image" accept="image/*"></p>
+    <p><input type="submit" value="Загрузить" name="submit"></p>
+</form>
+<!--проверяем если есть наша функция то получаем массив элементов в нем-->
+<?php if((is_dir($uploadDir)) and ($arr =scandir($uploadDir))): ?>
+    <table>
+        <tr>
+            <?php $count = 0; foreach($arr as $item){
+                if (!is_dir($uploadDir.$item)){ // проверяем что файл не директория
+                    if (exif_imagetype($uploadDir.$item)!=false){// проверяем что файл изображения
+                        if ($count%5===0){// если больше 5 картинок в строке, то начинаеться новая строка
+                            echo "</tr><tr>";
                         }
-                        ?>
-                    </td>
-            <?php endforeach ?>
-            </tr>
-        </table>
-    <?php endif ?>
+                        $url = $uploadDir.$item;
+                        echo '<td><a href="'.$url.'"><img src="'.$uploadDir.$item.'" ></a></td>';
+                        $count++;
+                    }
+                }
+            }
+            ?>
+        </tr>
+    </table>
+<?php endif ?>
+<script>
+
+</script>
 </body>
 </html>
