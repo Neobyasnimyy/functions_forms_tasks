caa331e42d0d7037ec783c61b501e4e37f7a5c22
Borysiuk Andrey <factodessa@gmail.com>
2017-07-19 23:16:33 +0300

    11 исправил ошибку

--- 11/11.php
+++ 11/11.php
@@ -17,20 +17,23 @@
 <body>
 <p>Функция каждое новое предложение начинает с большой буквы.</p>
 <form action="11.php" method="GET" accept-charset="UTF-8">
-    <p><textarea rows="8" cols="45" name="text" placeholder="Введите текст" ></textarea><br></p>
+    <p><textarea rows="8" cols="45" name="text" placeholder="Введите текст" >а васька слушает да ест. а воз и ныне там. а вы друзья как ни садитесь, все в музыканты не годитесь. а король-то — голый. а ларчик просто открывался.а там хоть трава не расти.</textarea><br></p>
     <p><input type="submit" name="submit" value="Отправить" ></p>
 </form>
 <?php
 
+
 if (isset($_GET['text'])) {
 
     $string = $_GET['text'];
-    $array = mb_split("\.", $string);
+    $array = mb_split("\.\s", $string);
     $new_string = '';
     foreach ($array as &$item) {
+        $item=rtrim($item,'.');
+        print $item."--<br>";
+
         $words = mb_split('\s', $item);
         foreach ($words as &$word) {
-
             if ($word!=" " and !empty($word)){
                 $word=mb_convert_case($word, MB_CASE_TITLE, "UTF-8");
 //                var_dump($word);
@@ -39,10 +42,10 @@ if (isset($_GET['text'])) {
                 break 1;
             }
         }
-    $new_string.=implode(" ", $words).".";
+    $new_string.=implode(" ", $words).". ";
     }
+    echo "<p>Результат: $new_string </p>";
 }
 ?>
-<p>Результат: <?php echo $new_string?></p>
 </body>
 </html>
